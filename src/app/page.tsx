98035826f885528cb6dd473db0c715ab3d40d26f
shoaibarain78 <shoaib786ali786arain@gmail.com>
2024-12-17 00:54:53 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+
+
+import Herro from "./components/herro";
+import FreshSales from "./components/freshSales";
+import Category from "./components/category";
+import Products from "./components/product";
+import Music from "./components/music";
+import Explore from "./components/explore";
+import Futured from "./components/fetured";
+import Freedelvry from "./components/free";
+
+
+export default function Home() {
+    return(
+      <div >
+        
+         <Herro/>
+         <FreshSales/>
+         <Category/>
+         <Products/>
+         <Music/>
+         <Explore/>
+         <Futured/>
+         <Freedelvry/>
+      </div>
+    )
+}
